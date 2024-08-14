3e68d71969dcc8c60a17c60836fc3f507b3c634b
Administrator <Administrator@eglorg3.com>
2024-08-14 09:51:24 +0100

    feature: template solid

--- app/src/main/java/com/text/card/core/ColorData.kt
+++ app/src/main/java/com/text/card/core/ColorData.kt
@@ -6,12 +6,50 @@ data class ColorData(
     var selected: Boolean = false
 ) {
 
+
+    // ColorData("", mutableListOf("#", "#"))
     companion object {
         //template media
         val BLUE_CYAN_GRADIENT =
             ColorData("Blue cyan gradient", mutableListOf("#78dac5", "#60a5f9"))
         val BLUE_PINK_GRADIENT =
             ColorData("Blue pink gradient", mutableListOf("#d2c9d9", "#39befa"))
+
+        //pure light
+        val CYAN = ColorData("Cyan", mutableListOf("#07b6d1", "#07b6d1"))
+        val LIGHT_CYAN = ColorData("Light cyan", mutableListOf("#24d1ec", "#24d1ec"))
+        val BLUE = ColorData("Blue", mutableListOf("#3c82f6", "#3c82f6"))
+        val LIGHT_BLUE = ColorData("Light blue", mutableListOf("#60A5FA", "#60A5FA"))
+        val TEAL = ColorData("Teal", mutableListOf("#15B8A6", "#15B8A6"))
+        val GREEN = ColorData("Green", mutableListOf("#22c55d", "#22c55d"))
+        val LIGHT_GREEN = ColorData("Light green", mutableListOf("#49DE80", "#49DE80"))
+        val EMERALD = ColorData("Emerald", mutableListOf("#12b980", "#12b980"))
+        val ROSE = ColorData("Rose", mutableListOf("#F87171", "#F87171"))
+        val LIGHT_ROSE = ColorData("Light rose", mutableListOf("#F471B7", "#F471B7"))
+        val LIGHT_PURPLE = ColorData("Light purple", mutableListOf("#c084fc", "#c084fc"))
+        val ORANGE = ColorData("Orange", mutableListOf("#f97316", "#f97316"))
+        val LIGHT_ORANGE = ColorData("Light orange", mutableListOf("#fb923c", "#fb923c"))
+        val YELLOW = ColorData("Yellow", mutableListOf("#facc14", "#facc14"))
+        val WHITE = ColorData("White", mutableListOf("#e4e4e7", "#e4e4e7"))
+
+        //pure dark
+        val DARK_GRAY_RED = ColorData("Gray red", mutableListOf("#7F1C1E", "#7F1C1E"))
+        val DARK_GRAY_GREEN = ColorData("Gray green", mutableListOf("#14532D", "#14532D"))
+        val DARK_GRAY_VIOLET = ColorData("Gray violit", mutableListOf("#250f4c", "#250f4c"))
+        val DARK_GRAY_INDIGO = ColorData("Gray indigo", mutableListOf("#181637", "#181637"))
+        val DARK_GRAY_BLUE = ColorData("Gray blue", mutableListOf("#172554", "#172554"))
+        val DARK_GRAY_CYAN = ColorData("Gray cyan", mutableListOf("#093344", "#093344"))
+        val DARK_DARK_CYAN = ColorData("Dark cyan", mutableListOf("#0a191f", "#0a191f"))
+        val DARK_DARK_INDIGO = ColorData("Dark indigo", mutableListOf("#07060f", "#07060f"))
+        val DARK_DARK_GREEN = ColorData("Dark green", mutableListOf("#021b0e", "#021b0e"))
+        val DARK_DARK_BLUE = ColorData("Dark blue", mutableListOf("#010512", "#010512"))
+        val DARK_DARK_VIOLET = ColorData("Dark violet", mutableListOf("#0d0816", "#0d0816"))
+        val DARK_LIGHT_WARM_GRAY = ColorData("Light warm gray", mutableListOf("#262626", "#262626"))
+        val DARK_LIGHT_COLD_GRAY = ColorData("Light cold gray", mutableListOf("#1e293b", "#1e293b"))
+        val DARK_BLACK = ColorData("Black", mutableListOf("#0a0a0a", "#0a0a0a"))
+
+        //ColorData("", mutableListOf("#", "#"))
+
 
         val DARK_BLUE_GRADIENT =
             ColorData("Dark blue gradient", mutableListOf("#14214d", "#061735"))
--- app/src/main/java/com/text/card/template/TemplateSolid.kt
+++ app/src/main/java/com/text/card/template/TemplateSolid.kt
@@ -8,31 +8,56 @@ import com.text.card.R
 import com.text.card.core.ColorData
 import com.text.card.core.TemplateBgColor
 import com.text.card.core.TemplateModel
-import com.text.card.databinding.TemplateMediaBinding
+import com.text.card.databinding.TemplateSolidBinding
 import com.text.card.helper.log
 import com.text.card.ui.widget.GradientBackgroundDrawable
 
-class TemplateSolid : TemplateModel<TemplateMediaBinding>() {
+class TemplateSolid : TemplateModel<TemplateSolidBinding>() {
     private val colorList = mutableListOf(
         TemplateBgColor(
             //light color
             mutableListOf(
-                ColorData.BLUE_CYAN_GRADIENT,
-                ColorData.BLUE_PINK_GRADIENT
+                ColorData.CYAN,
+                ColorData.LIGHT_CYAN,
+                ColorData.BLUE,
+                ColorData.LIGHT_BLUE,
+                ColorData.TEAL,
+                ColorData.GREEN,
+                ColorData.LIGHT_GREEN,
+                ColorData.EMERALD,
+                ColorData.ROSE,
+                ColorData.LIGHT_ROSE,
+                ColorData.LIGHT_PURPLE,
+                ColorData.ORANGE,
+                ColorData.LIGHT_ORANGE,
+                ColorData.YELLOW,
+                ColorData.WHITE
             ),
             TemplateBgColor.COLOR_LIGHT,
         ),
         //dark color
         TemplateBgColor(
             mutableListOf(
-                ColorData.DARK_BLUE_GRADIENT,
-                ColorData.DARK_INDIGO_GRADIENT
+                ColorData.DARK_GRAY_RED,
+                ColorData.DARK_GRAY_GREEN,
+                ColorData.DARK_GRAY_VIOLET,
+                ColorData.DARK_GRAY_INDIGO,
+                ColorData.DARK_GRAY_BLUE,
+                ColorData.DARK_GRAY_CYAN,
+                ColorData.DARK_DARK_CYAN,
+                ColorData.DARK_DARK_INDIGO,
+                ColorData.DARK_DARK_GREEN,
+                ColorData.DARK_DARK_BLUE,
+                ColorData.DARK_DARK_VIOLET,
+                ColorData.DARK_LIGHT_WARM_GRAY,
+                ColorData.DARK_LIGHT_COLD_GRAY,
+                ColorData.DARK_BLACK
             ),
             TemplateBgColor.COLOR_DARK,
         )
     )
 
-    override fun inflateView() = TemplateMediaBinding.inflate(LayoutInflater.from(App.context))
+    override fun inflateView() = TemplateSolidBinding.inflate(LayoutInflater.from(App.context))
     override fun getTemplateName(): String {
         return "Solid"
     }
@@ -96,44 +121,40 @@ class TemplateSolid : TemplateModel<TemplateMediaBinding>() {
             }
 
             //cardView
-            val cardBgColor = ContextCompat.getColor(
-                App.context,
-                if (isDark) R.color.card_drak_bg_color else R.color.white
-            )
-            cardView.setCardBackgroundColor(cardBgColor)
+            cardView.setBackgroundResource(if (isDark) R.drawable.shape_solid_bg_dark else R.drawable.shape_solid_bg)
 
             //icon
-            ivIcon.setBackgroundResource(if (isDark) R.drawable.shape100f17_r45 else R.drawable.shape_999999_r45)
+//            ivIcon.setBackgroundResource(if (isDark) R.drawable.shape100f17_r45 else R.drawable.shape_999999_r45)
             ivIcon.setColorFilter(
                 ContextCompat.getColor(
                     App.context,
-                    if (isDark) R.color.white else R.color.black
+                    if (isDark) R.color.color_A0ffffff else R.color.color_A0000000
                 )
             )
 
             //Date
-            tvDate.setTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
+            tvDate.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
 
             //title
-            etTitle.setTextColor(App.getColor(if (isDark) R.color.white else R.color.black))
-            etTitle.setHintTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_999999))
+            etTitle.setTextColor(App.getColor(if (isDark) R.color.color_A0ffffff else R.color.color_A0000000))
+            etTitle.setHintTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
             //content
-            etContent.setTextColor(App.getColor(if (isDark) R.color.white else R.color.black))
-            etContent.setHintTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_999999))
+            etContent.setTextColor(App.getColor(if (isDark) R.color.color_A0ffffff else R.color.color_A0000000))
+            etContent.setHintTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
 
             //author
-            etAuthor.setTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
-            etAuthor.setHintTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
+            etAuthor.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
+            etAuthor.setHintTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_80000000))
             //word
-            tvWordCount.setTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
+            tvWordCount.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
 
             //qrcode
-            ivQrCode.setColorFilter(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
-            tvQrCodeTitle.setTextColor(App.getColor(if (isDark) R.color.color_4C5159 else R.color.color_999999))
-            tvQrCodeDesc.setTextColor(App.getColor(if (isDark) R.color.color_41404A else R.color.color_B0B5B9))
+            ivQrCode.setColorFilter(App.getColor(if (isDark) R.color.color_A0ffffff else R.color.color_40000000))
+            tvQrCodeTitle.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
+            tvQrCodeDesc.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_20000000))
 
             //water mark
-            tvWater.setTextColor(App.getColor(if (isDark) R.color.color_4D4E5D else R.color.color_999999))
+            tvWater.setTextColor(App.getColor(if (isDark) R.color.color_40ffffff else R.color.color_40000000))
 
         }
     }
